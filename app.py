13656bef82dca07d2c2e68537ac1fa9206f02f6f
ketann26 <ketan9sharma@yahoo.in>
2021-12-11 03:18:49 +0530

    New Routes added

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask, render_template
-import yfinance as yf
+from login import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '9ad31e48816f68f953e9214654a140a7'
 
 @app.route("/")
 def root():
     return render_template('index.html')
+
+@app.route("/register")
+def register():
+
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
